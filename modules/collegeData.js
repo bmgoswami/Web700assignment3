bbdea9ea609e0b0e63b3312961264f92450eb248
bmgoswami <bmgoswami@myseneca.ca>
2024-06-23 11:37:03 -0400

    Final Update

--- modules/collegeData.js
+++ modules/collegeData.js
@@ -11,12 +11,12 @@ let dataCollection = null;
 
 module.exports.initialize = function () {
     return new Promise( (resolve, reject) => {
-        fs.readFile('./data/courses.json','utf8', (err, courseData) => {
+        fs.readFile('./Assignment-3/data/courses.json','utf8', (err, courseData) => {
             if (err) {
                 reject("unable to load courses"); return;
             }
 
-            fs.readFile('./data/students.json','utf8', (err, studentData) => {
+            fs.readFile('./Assignment-3/data/students.json','utf8', (err, studentData) => {
                 if (err) {
                     reject("unable to load students"); return;
                 }
